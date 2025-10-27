6ca6b5daee331400137c014d815b376f5934f7c6
Luan Fiszer <Luan Fiszer@DESKTOP-TCF5TJH>
2025-10-27 16:49:10 -0300

    .

--- AvaliaFilmesAPI.Domain/Models/Avaliacao.cs
+++ AvaliaFilmesAPI.Domain/Models/Avaliacao.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AvaliaFilmesAPI.Domain.Models.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AvaliaFilmesAPI.Domain.Entities
 {
